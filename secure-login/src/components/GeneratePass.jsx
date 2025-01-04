5d0ec18d6603ecb0889770b17a5a3cb969990e50
MontesJJ <javimontes91@gmail.com>
2025-01-04 11:08:20 +0100

    FEAT: Save the pass generated localy

--- secure-login/src/components/GeneratePass.jsx
+++ secure-login/src/components/GeneratePass.jsx
@@ -1,23 +1,44 @@
+import { useState, useEffect } from 'react'
+
 export const GeneratePass = () => {
 
-    let _datos = {
-        "length": 12,
-        "includeNumbers": true,
-        "includeSymbols": true,
-        "includeUppercase": true,
-        "excludeSimilar": false
-    }
+    const [password, setPassword] = useState(null);
 
-    fetch('http://localhost:4000/api/generate', {
-        method: "POST",
-        body: JSON.stringify(_datos),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-    }) 
-        .then(response => response.json())
-        .then(json => console.log(json))
+    useEffect(() => {
+
+        let _datos = {
+            "length": 12,
+            "includeNumbers": true,
+            "includeSymbols": true,
+            "includeUppercase": true,
+            "excludeSimilar": false
+        }
+
+        const apiUrl = 'http://localhost:4000/api/generate';
+
+        fetch(apiUrl, {
+            method: "POST",
+            body: JSON.stringify(_datos),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        }) 
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error ('Se ha producido un error de conexión')
+                }
+                return response.json()
+            })
+            .then(json => {
+                setPassword(json);
+                console.log(json)
+            })
+            .catch (error => {
+                error.log("Error: ", error)
+            })
+
+        }, []);
 
     return (
-        <h1>HOLA</h1>
+        <h1>Pass: {password.password}</h1>
     )
 
 }
